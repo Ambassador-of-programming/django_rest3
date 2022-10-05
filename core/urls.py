a6583a1fab92dd81b26db6c1596ef3ea0dc1ccea
aktifon <aktifman8@gmail.com>
2022-10-05 12:52:00 +0600

    all

--- core/urls.py
+++ core/urls.py
@@ -20,3 +20,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('mainapp.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -1,4 +1,3 @@
-from itertools import product
 from rest_framework import serializers
 
 from mainapp.models import(
@@ -12,11 +11,11 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class CartSerializer(serializers.ModelSerializer):
-    product = serializers.ListField()
+    product = serializers.ListField(write_only=True)
     class Meta:
         model = Cart
         fields = ('id', 'user_name', 'created_at', 
-        'total_price', 'delivery_address', )
+        'total_price', 'delivery_address', 'product', )
     read_only_fields = ('created_at', )
 
     def create(self, validated_data):
@@ -34,6 +33,15 @@ class CartSerializer(serializers.ModelSerializer):
         cart.total_price = sum([p.sum_product_price for p in cart.cart_product.all()])
         cart.save()
         return cart
+
+    def to_representntation(self, instance):
+        return {
+            'user_name': instance.user_name,
+            'delivery_address': instance.delivery_address,
+            'created_at': instance.created_at,
+            'total_price': instance.total_price,
+            'product': instance.product,
+        }
 
     # [
     #     {'id': 1, 'amout': 10},
